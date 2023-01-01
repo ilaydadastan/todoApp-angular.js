ff598d2d5a82c800f47a409a57c7ad1324ab22b9
ilaydadastan <ilaydastan2@gmail.com>
2023-01-01 23:50:45 +0200

    added actions with ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Model, TodoItem} from "./model";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoApp';
+  model = new Model();
+  isDisplay = false;
+
+  getName(){
+    return this.model.user;
+  }
+  getItems(){
+    return this.model.items.filter(item=> this.isDisplay || !item.action );
+  }
+  addItem(value:string){
+    if (value!=''){
+      this.model.items.push(new TodoItem(value, false));
+    }
+  }
 }
---
+++ src/app/model.ts
@@ -0,0 +1,25 @@
+export class Model {
+  user;
+  items;
+
+  constructor() {
+    this.user = "İlayda";
+    this.items = [
+      new TodoItem("Spor", true),
+      new TodoItem("Kahvaltı", false),
+      new TodoItem("Kitap Okumak", false),
+      new TodoItem("Sinema", false)
+    ];
+  }
+}
+
+export class TodoItem {
+  description;
+  action;
+
+  constructor(description: string, action: boolean) {
+    this.description = description;
+    this.action = action;
+  }
+
+}
